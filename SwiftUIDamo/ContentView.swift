69ba58e57f0bc843a2b665e84111d06f6870aa94
freak_lather <ajaysinghlather@gmail.com>
2020-04-01 18:35:35 +0800

    initial commit

--- SwiftUIDamo/ContentView.swift
+++ SwiftUIDamo/ContentView.swift
@@ -5,12 +5,45 @@
 //  Created by ajay lather on 01/04/20.
 //  Copyright © 2020 Ajay Lather. All rights reserved.
 //
-
+import Combine
 import SwiftUI
 
-struct ContentView: View {
+class DataSource: ObservableObject {
+    let didChange = PassthroughSubject<Void, Never>()
+    var pictures = [String]()
+    init() {
+        let fm = FileManager.default
+        if let path = Bundle.main.resourcePath, let items = try? fm.contentsOfDirectory(atPath: path) {
+            for item in items {
+                if item.hasPrefix("IMG") {
+                    pictures.append(item)
+                }
+            }
+        }
+        didChange.send(())
+    }
+    
+}
+struct DetailView: View {
+    var selectedImage: String
     var body: some View {
-        Text("Hello, World!")
+        let image = UIImage(named: selectedImage)!
+        return Image(uiImage: image)
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .navigationBarTitle(Text(selectedImage))
+    }
+}
+struct ContentView: View {
+    @ObservedObject var dataSource = DataSource()
+    var body: some View {
+        NavigationView {
+            List(dataSource.pictures, id:\.self) { picture in
+                NavigationLink(destination: DetailView(selectedImage: picture)) {
+                   Text(picture)
+                }
+            }.navigationBarTitle("Desa Green Apartment")
+        }
     }
 }
 
